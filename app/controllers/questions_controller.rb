6650cb8393cf103269419adc240610851c82a813
Pete Kreppein <pmkreppein@gmail.com>
2019-01-03 02:34:33 -0500

    Ask a question form working and submitting properly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,12 @@ class QuestionsController < ApplicationController
     def index
         @questions = Question.all
     end
+
+    def new
+        @question = Question.new
+    end
+
+    def create
+        raise params.inspect
+    end
 end

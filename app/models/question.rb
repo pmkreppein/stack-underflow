7999b27672cf4f649781091f7ffcc1d78e0e6b7c
Pete Kreppein <pmkreppein@gmail.com>
2019-01-05 20:55:25 -0500

    Add Category DB field and Model validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
     has_many :answers
     has_many :users, through: :answers
+
+    CATEGORY = ["Ruby/Rails", "JavaScript", "Front End Frameworks", "Native Mobile Development", "JS Mobile Development", "Game/VR Development"]
+
+    validates :category, inclusion: CATEGORY
+    validates :question_text, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_060042) do
+ActiveRecord::Schema.define(version: 2019_01_06_015048) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_01_03_060042) do
     t.text "question_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "users", force: :cascade do |t|

9cde36c06b0785fb97906f3a8f7d008f891ea597
Pete Kreppein <pmkreppein@gmail.com>
2019-01-21 15:50:17 -0500

    Application js changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require handlebars.runtime
+//= require handlebars
+//= require jquery
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
